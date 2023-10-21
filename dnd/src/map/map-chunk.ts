944e6290a31e5074117d110e4955d310d21a902d
jergauth <jeremiegthr@gmail.com>
2023-10-21 19:26:32 +0200

    refactor: MapChunk ctor

--- dnd/src/map/map-chunk.ts
+++ dnd/src/map/map-chunk.ts
@@ -4,10 +4,10 @@ import { Tile } from './tile';
 export class MapChunk {
   private static SIZE = 11 ** 2;
 
-  public readonly tiles: Tile[];
+  constructor(public readonly tiles = MapChunk.generateEmptyMapChunk()) {}
 
-  constructor() {
-    this.tiles = Array.from({ length: MapChunk.SIZE }).map(
+  private static generateEmptyMapChunk() {
+    return Array.from({ length: MapChunk.SIZE }).map(
       (_, index) => new Tile('free', undefined, `${index}`),
     );
   }
