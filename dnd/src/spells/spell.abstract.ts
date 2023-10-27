5b2fb314a3fa8d47905d86e101fceab211e7ffec
jergauth <jeremiegthr@gmail.com>
2023-10-27 10:13:03 +0200

    feat: add spells

---
+++ dnd/src/spells/spell.abstract.ts
@@ -0,0 +1,31 @@
+import type { Dice, DiceRoll } from '../dices/dice.abstract';
+import { AttackType } from '../interfaces/attack-type.enum';
+import type { SpellCaster } from '../interfaces/character-class.type';
+import { Item } from '../inventory/item.abstract';
+
+export abstract class Spell implements Item {
+  readonly type: 'weapon' | 'artifact' | 'spell' = 'spell';
+  abstract readonly name: string;
+  abstract readonly description: string;
+  abstract readonly attackType: AttackType;
+  abstract readonly perks?: 're-roll' | 'super attack';
+  abstract readonly dices: Dice[];
+  abstract readonly superAttackDices?: Dice[];
+  abstract readonly manaCost: Readonly<Record<SpellCaster, number>>;
+
+  public rollAttack(): DiceRoll[] {
+    return this.dices.map((dice) => dice.roll());
+  }
+
+  public rollSuperAttack(): DiceRoll[] {
+    if (!this.superAttackDices) {
+      throw new Error(`No super attack for this weapon`);
+    }
+
+    return this.superAttackDices.map((dice) => dice.roll());
+  }
+
+  public rerollDice(dice: Dice): DiceRoll {
+    return dice.roll();
+  }
+}
