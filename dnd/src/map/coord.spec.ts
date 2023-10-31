4618914ab457ce9851c12814d85f2d62a9bce0f1
jergauth <jeremiegthr@gmail.com>
2023-10-31 18:31:00 +0100

    test: add coord spec

---
+++ dnd/src/map/coord.spec.ts
@@ -0,0 +1,100 @@
+import { describe, expect, it } from 'vitest';
+import { Coord } from './coord';
+
+describe('map: Coord', () => {
+  describe('method: from', () => {
+    it('should creates an instance with the `from` method', () => {
+      const x = 21;
+      const y = 42;
+
+      const result = Coord.from({ x, y });
+      const expected = new Coord(x, y);
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe('method: equals', () => {
+    it('should returns true when coords share the same x,y values', () => {
+      const x = 21;
+      const y = 42;
+      const coordA = Coord.from({ x, y });
+      const coordB = new Coord(x, y);
+
+      const result = coordA.equals(coordB);
+      const resultOppositeSide = coordB.equals(coordA);
+      const expected = true;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+
+    it("should returns false when coords don't share the same x,y values", () => {
+      const coordA = Coord.from({ x: 21, y: 42 });
+      const coordB = new Coord(32, 53);
+
+      const result = coordA.equals(coordB);
+      const resultOppositeSide = coordB.equals(coordA);
+      const expected = false;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+  });
+
+  describe('method: isNextTo', () => {
+    it('should returns true when coords are next to each other', () => {
+      const x = 21;
+      const y = 42;
+      const coordA = Coord.from({ x, y });
+      const coordB = new Coord(x + 1, y);
+
+      const result = coordA.isNextTo(coordB);
+      const resultOppositeSide = coordB.isNextTo(coordA);
+      const expected = true;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+
+    it('should returns false when coords are in diagonal to each other', () => {
+      const x = 21;
+      const y = 42;
+      const coordA = Coord.from({ x, y });
+      const coordB = new Coord(x + 1, y + 1);
+
+      const result = coordA.isNextTo(coordB);
+      const resultOppositeSide = coordB.isNextTo(coordA);
+      const expected = false;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+
+    it('should returns false when coords share the same x,y values', () => {
+      const x = 21;
+      const y = 42;
+      const coordA = Coord.from({ x, y });
+      const coordB = new Coord(x, y);
+
+      const result = coordA.isNextTo(coordB);
+      const resultOppositeSide = coordB.isNextTo(coordA);
+      const expected = false;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+
+    it('should returns false when coords are distant to each other', () => {
+      const coordA = Coord.from({ x: 21, y: 42 });
+      const coordB = new Coord(32, 53);
+
+      const result = coordA.isNextTo(coordB);
+      const resultOppositeSide = coordB.isNextTo(coordA);
+      const expected = false;
+
+      expect(result).toBe(expected);
+      expect(resultOppositeSide).toBe(expected);
+    });
+  });
+});
