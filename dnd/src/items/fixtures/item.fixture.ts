05caf85925f09cc1427bdb133254ba5f0d112150
jergauth <jeremiegthr@gmail.com>
2023-11-03 18:32:14 +0100

    test: add item spec

---
+++ dnd/src/items/fixtures/item.fixture.ts
@@ -0,0 +1,16 @@
+import { Item } from '../item.abstract';
+
+export class MockWeaponItem extends Item {
+  name = 'mock-weapon-item';
+  type: 'weapon' | 'artifact' | 'spell' = 'weapon';
+}
+
+export class MockSpellItem extends Item {
+  name = 'mock-spell-item';
+  type: 'weapon' | 'artifact' | 'spell' = 'spell';
+}
+
+export class MockArtifactItem extends Item {
+  name = 'mock-artifact-item';
+  type: 'weapon' | 'artifact' | 'spell' = 'artifact';
+}
