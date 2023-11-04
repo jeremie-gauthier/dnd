8ed8f533cb3203ba3a914298dfabf252282818fd
jergauth <jeremiegthr@gmail.com>
2023-11-04 14:45:59 +0100

    config: disable logs while running tests

--- dnd/vitest.config.ts
+++ dnd/vitest.config.ts
@@ -2,6 +2,9 @@ import { defaultExclude, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    onConsoleLog() {
+      return false;
+    },
     include: ['**/*.spec.ts'],
     coverage: {
       provider: 'v8',
