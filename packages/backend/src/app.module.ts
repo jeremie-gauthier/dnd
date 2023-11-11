48eac7d1e4edac0b664f9c6b576a3c448be128ad
jergauth <jeremiegthr@gmail.com>
2023-11-11 08:28:09 +0100

    feat: add env config

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import envConfig, { validationSchema } from './config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfig],
+      validationSchema,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/backend/src/config/env.config.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export default () => ({
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+});
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+});
