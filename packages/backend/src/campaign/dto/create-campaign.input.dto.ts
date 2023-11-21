8c63fa73cf160139271ba2830e852a7e2a7be8d2
jergauth <jeremiegthr@gmail.com>
2023-11-21 23:40:27 +0100

    refacto: strict input schemas

--- packages/backend/src/campaign/dto/create-campaign.input.dto.ts
+++ packages/backend/src/campaign/dto/create-campaign.input.dto.ts
@@ -1,13 +1,18 @@
 import { createZodDto } from 'nestjs-zod';
 import z from 'zod';
 
-const stageSchema = z.object({
-  intro: z.string(),
-  outro: z.string(),
-});
-const createCampaignInputSchema = z.object({
-  title: z.string(),
-  stages: z.array(stageSchema),
-});
+const stageSchema = z
+  .object({
+    intro: z.string(),
+    outro: z.string(),
+  })
+  .strict();
+
+const createCampaignInputSchema = z
+  .object({
+    title: z.string(),
+    stages: z.array(stageSchema),
+  })
+  .strict();
 
 export class CreateCampaignInputDTO extends createZodDto(createCampaignInputSchema) {}
