accaed506b2cb147b0f9eb330ebbf262e86c7292
jergauth <jeremiegthr@gmail.com>
2023-11-23 19:37:08 +0100

    feat: add event emitter pkg

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CampaignModule } from './campaign/campaign.module';
 import envConfig, { validate } from './config/env.config';
 import { DatabaseModule } from './database/database.module';
-import { WsEventsModule } from './ws-events/ws-events.module';
-import { ItemModule } from './item/item.module';
 import { EntityModule } from './entity/entity.module';
+import { ItemModule } from './item/item.module';
+import { WsEventsModule } from './ws-events/ws-events.module';
 
 @Module({
   imports: [
@@ -17,6 +18,9 @@ import { EntityModule } from './entity/entity.module';
       load: [envConfig],
       validate,
       cache: true,
+    }),
+    EventEmitterModule.forRoot({
+      wildcard: true,
     }),
     WsEventsModule,
     DatabaseModule,
