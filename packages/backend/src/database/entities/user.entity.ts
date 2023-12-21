c93903b670e66efabb1b9c022628f8a5a8c3af6e
jergauth <jeremiegthr@gmail.com>
2023-12-21 20:33:08 +0100

    feat: add user status

--- packages/backend/src/database/entities/user.entity.ts
+++ packages/backend/src/database/entities/user.entity.ts
@@ -1,10 +1,18 @@
-import { Entity, OneToMany, PrimaryColumn, Relation } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryColumn, Relation } from 'typeorm';
 import { CampaignProgression } from './campaign-progression.entity';
+
+export enum UserStatus {
+  CREATED = 'CREATED',
+  INITIALIZED = 'INITIALIZED',
+}
 
 @Entity()
 export class User {
   @PrimaryColumn()
   readonly id: string;
+
+  @Column({ type: 'enum', enum: UserStatus })
+  status: UserStatus;
 
   @OneToMany(() => CampaignProgression, (campaignProgression) => campaignProgression.user, {
     onDelete: 'CASCADE',
