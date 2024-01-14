d019ebd754c901612daafe6f0b7e6ab4c0aef331
jergauth <jeremiegthr@gmail.com>
2024-01-14 17:07:01 +0100

    feat: allow dnd to run on local network

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,9 +1,12 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
+const isRunningOnLocalNetwork = process.env.VITE_ENV === 'local_network';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), isRunningOnLocalNetwork && basicSsl()],
   optimizeDeps: {
     include: ['@dnd/shared'],
   },
