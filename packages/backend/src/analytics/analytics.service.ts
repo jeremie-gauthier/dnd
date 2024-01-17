022211eb38b63e0a5a8ef2463a592ea5803aa856
jergauth <jeremiegthr@gmail.com>
2024-01-17 10:09:09 +0100

    feat: log only event name on analytics module

--- packages/backend/src/analytics/analytics.service.ts
+++ packages/backend/src/analytics/analytics.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { EventPayload } from 'src/event-emitter/event-payload.class';
 import { AnalyticsEvent } from './events/types/events';
 
 @Injectable()
@@ -7,7 +8,7 @@ export class AnalyticsService {
   constructor(private readonly eventEmitter: EventEmitter2) {}
 
   @OnEvent(AnalyticsEvent.OnAnyEvent, { async: true })
-  public handleInteraction(data: unknown) {
-    console.log('spying event', data);
+  public handleInteraction(data: EventPayload<any>) {
+    console.log('spying event', data.name);
   }
 }
