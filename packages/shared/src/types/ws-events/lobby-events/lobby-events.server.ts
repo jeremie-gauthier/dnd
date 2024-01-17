17b47fbc6bd52e037e28f865e147c22725270931
jergauth <jeremiegthr@gmail.com>
2024-01-17 21:25:18 +0100

    feat: add lobbies deleted server event

--- packages/shared/src/types/ws-events/lobby-events/lobby-events.server.ts
+++ packages/shared/src/types/ws-events/lobby-events/lobby-events.server.ts
@@ -1,5 +1,6 @@
 export const ServerLobbyEvent = {
   LobbiesChangesDetected: 'server.lobbies.changes_detected',
+  LobbiesDeleted: 'server.lobbies.deleted',
   GameReady: 'server.lobby.game_is_ready',
   Error: 'server.lobby.error',
 } as const;
--- packages/shared/src/types/ws-events/server-to-client-events.ts
+++ packages/shared/src/types/ws-events/server-to-client-events.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { getLobbiesOutputSchema } from '../../schemas';
+import { LobbyEntity } from '../lobby';
 import { ServerGameEvent } from './game-events/game-events.server';
 import { ServerLobbyEvent } from './lobby-events/lobby-events.server';
 import { EventsMapper } from './utils.type';
@@ -8,6 +9,7 @@ interface ServerToClientEventsAndPayloads extends Record<string, (...parameters:
   [ServerLobbyEvent.LobbiesChangesDetected]: (payload: {
     lobby: z.infer<typeof getLobbiesOutputSchema>[number];
   }) => void;
+  [ServerLobbyEvent.LobbiesDeleted]: (payload: { lobbyId: LobbyEntity['id'] }) => void;
   [ServerLobbyEvent.GameReady]: () => void;
   [ServerLobbyEvent.Error]: (payload: { name: string; message: string }) => void;
   [ServerGameEvent.GameStart]: () => void;
