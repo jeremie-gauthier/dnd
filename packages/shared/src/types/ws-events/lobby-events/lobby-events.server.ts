f787851011f409387053e545bbf82c3040cb87b0
jergauth <jeremiegthr@gmail.com>
2024-01-17 11:12:30 +0100

    feat: add event lobbies changes

--- packages/shared/src/types/ws-events/lobby-events/lobby-events.server.ts
+++ packages/shared/src/types/ws-events/lobby-events/lobby-events.server.ts
@@ -1,4 +1,5 @@
 export const ServerLobbyEvent = {
+  LobbiesChangesDetected: 'server.lobbies.changes_detected',
   GameReady: 'server.lobby.game_is_ready',
   Error: 'server.lobby.error',
 } as const;
--- packages/shared/src/types/ws-events/server-to-client-events.ts
+++ packages/shared/src/types/ws-events/server-to-client-events.ts
@@ -1,8 +1,12 @@
+import { LobbyEntity } from '../lobby';
 import { ServerGameEvent } from './game-events/game-events.server';
 import { ServerLobbyEvent } from './lobby-events/lobby-events.server';
 import { EventsMapper } from './utils.type';
 
 interface ServerToClientEventsAndPayloads extends Record<string, (...parameters: any[]) => any> {
+  [ServerLobbyEvent.LobbiesChangesDetected]: (payload: {
+    lobby: { id: LobbyEntity['id'] } & Partial<LobbyEntity>;
+  }) => void;
   [ServerLobbyEvent.GameReady]: () => void;
   [ServerLobbyEvent.Error]: (payload: { name: string; message: string }) => void;
   [ServerGameEvent.GameStart]: () => void;
