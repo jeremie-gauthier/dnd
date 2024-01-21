6a20c2dc4ed84ab6c27cd490ed21c50bfdb95648
jergauth <jeremiegthr@gmail.com>
2024-01-21 20:10:19 +0100

    refacto(ws-dc): lift event emitter in uc

--- packages/backend/src/lobby/private/handle-ws-disconnection/handle-ws-disconnection.repository.ts
+++ packages/backend/src/lobby/private/handle-ws-disconnection/handle-ws-disconnection.repository.ts
@@ -1,17 +1,12 @@
 import { LobbyEntity } from '@dnd/shared';
 import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { User } from 'src/database/entities/user.entity';
-import { LobbyEvent } from 'src/lobby/events/emitters/lobby-events.enum';
-import { UserForceLeftLobbyPayload } from 'src/lobby/events/emitters/user-force-left-lobby.payload';
 import { LobbiesRepository } from 'src/redis/repositories/lobbies.repository';
 import { UsersRepository } from 'src/redis/repositories/users.repository';
-import { MessageContext } from 'src/types/socket.type';
 
 @Injectable()
 export class HandleWsDisconnectionRepository {
   constructor(
-    private readonly eventEmitter: EventEmitter2,
     private readonly usersRepository: UsersRepository,
     private readonly lobbiesRepository: LobbiesRepository,
   ) {}
@@ -25,11 +20,9 @@ export class HandleWsDisconnectionRepository {
   }
 
   public async removePlayerFromLobby({
-    ctx,
     userId,
     lobbyId,
   }: {
-    ctx: MessageContext;
     userId: User['id'];
     lobbyId: LobbyEntity['id'];
   }): Promise<void> {
@@ -42,14 +35,5 @@ export class HandleWsDisconnectionRepository {
       ...lobby,
       players: lobby.players.filter((player) => player.userId !== userId),
     });
-
-    this.eventEmitter.emit(
-      LobbyEvent.UserForceLeftLobby,
-      new UserForceLeftLobbyPayload({
-        ctx,
-        userId,
-        lobbyId,
-      }),
-    );
   }
 }
