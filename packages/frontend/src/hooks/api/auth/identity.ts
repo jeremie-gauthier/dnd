8bf0d212d70c557d6f7347abcdb30a8e01fd76f2
jergauth <jeremiegthr@gmail.com>
2024-01-29 00:13:32 +0100

    fix: connection notif from front

--- packages/frontend/src/hooks/api/auth/identity.ts
+++ packages/frontend/src/hooks/api/auth/identity.ts
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { useQuery } from '@tanstack/react-query';
-import { fetcherWithAuth } from '../../config/fetcher';
+import { fetcherWithAuth } from '../../../config/fetcher';
 
 type IdentityResponse = {
   id: string;
