996b92881759a2d1c2e0e6b977233119a4a8ca5e
jergauth <jeremiegthr@gmail.com>
2024-02-10 11:37:13 +0100

    feat(script): generate spec file when creating uc

---
+++ scripts/new-uc/snippets/use-case-spec.snippet.js
@@ -0,0 +1,60 @@
+const dedent = require("dedent");
+
+const renderUseCaseSpec = ({
+  useCaseName,
+  useCaseClassName,
+  repositoryName,
+}) => {
+  return dedent`
+		import { EventEmitter2 } from '@nestjs/event-emitter';
+		import { Test } from '@nestjs/testing';
+		import { MockInstance, afterEach, beforeEach, describe, vi } from 'vitest';
+		import { ${repositoryName} } from './${useCaseName}.repository';
+		import { ${useCaseClassName} } from './${useCaseName}.uc';
+		
+		describe('${useCaseClassName}', () => {
+		  let useCase: ${useCaseClassName};
+		  let repository: ${repositoryName};
+		  let eventEmitter2: EventEmitter2;
+		
+		  let eventEmitterMock: MockInstance<[event: any, ...values: any[]], Promise<any[]>>;
+
+		  beforeEach(async () => {
+		    const module = await Test.createTestingModule({
+		      providers: [
+		        ${useCaseClassName},
+		        EventEmitter2,
+		        {
+		          provide: ${repositoryName},
+		          useValue: {},
+		        },
+		      ],
+		    }).compile();
+		
+		    useCase = module.get<${useCaseClassName}>(${useCaseClassName});
+		    repository = module.get<${repositoryName}>(${repositoryName});
+		    eventEmitter2 = module.get<EventEmitter2>(EventEmitter2);
+		
+		    eventEmitterMock = vi.spyOn(eventEmitter2, 'emitAsync');
+		  });
+		
+		  afterEach(() => {
+		    vi.clearAllMocks();
+		  });
+
+		  it('should be defined', () => {
+		    expect(useCase).toBeDefined();
+		    expect(repository).toBeDefined();
+		    expect(eventEmitter2).toBeDefined();
+		  });
+		
+		  describe('Happy path', () => {});
+
+		  describe('Negative path', () => {});
+		});\n
+	`;
+};
+
+module.exports = {
+  renderUseCaseSpec,
+};
