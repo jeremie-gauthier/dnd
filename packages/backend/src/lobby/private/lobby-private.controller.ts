61680ade51011087d17ef1b7d15b8633204c8b3c
jergauth <jeremiegthr@gmail.com>
2024-02-10 09:54:19 +0100

    feat: add explicit response type on controller

--- packages/backend/src/lobby/private/lobby-private.controller.ts
+++ packages/backend/src/lobby/private/lobby-private.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { JWTAuthGuard } from 'src/authz/jwt-auth.guard';
+import { GetLobbiesOutputDto } from './get-lobbies/get-lobbies.dto';
 import { GetLobbiesUseCase } from './get-lobbies/get-lobbies.uc';
+import { GetLobbyOutputDto } from './get-lobby/get-lobby.dto';
 import { GetLobbyUseCase } from './get-lobby/get-lobby.uc';
 
 @UseGuards(JWTAuthGuard)
@@ -12,12 +14,12 @@ export class LobbyPrivateController {
   ) {}
 
   @Get('get-lobbies')
-  public async getLobbies() {
+  public async getLobbies(): Promise<GetLobbiesOutputDto> {
     return await this.getLobbiesUseCase.execute();
   }
 
   @Get('get-lobby/:lobbyId')
-  public async getLobby(@Param('lobbyId') lobbyId: string) {
+  public async getLobby(@Param('lobbyId') lobbyId: string): Promise<GetLobbyOutputDto> {
     return await this.getLobbyUseCase.execute({ lobbyId });
   }
 }
