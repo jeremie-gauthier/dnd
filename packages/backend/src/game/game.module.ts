fc49c85a509231428bf78f806cf0409584a075ee
jergauth <jeremiegthr@gmail.com>
2024-02-28 23:29:04 +0100

    feat: create game starting module

--- packages/backend/src/game/game.module.ts
+++ packages/backend/src/game/game.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GameEventsGateway } from './game-events.gateway';
 import { MapModule } from './map/map.module';
+import { StartingModule } from './starting/starting.module';
 
 @Module({
-  imports: [MapModule],
+  imports: [MapModule, StartingModule],
   providers: [GameEventsGateway],
 })
 export class GameModule {}
