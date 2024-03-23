5e6725f471f1ac7f3869a27d4cfa20d7f9bc0cee
jergauth <jeremiegthr@gmail.com>
2024-03-23 20:13:54 +0100

    PoC auth token expiration

--- packages/frontend/src/hooks/api/auth/connection.ts
+++ packages/frontend/src/hooks/api/auth/connection.ts
@@ -1,14 +1,30 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from "@tanstack/react-router";
 import { fetcherWithAuth } from "../../../config/fetcher";
 
 type ConnectionResponse = "Ok";
 
 export const useConnection = () => {
-  const { getAccessTokenSilently } = useAuth0();
+  const router = useRouter();
+  const { getAccessTokenSilently, loginWithRedirect } = useAuth0();
+
+  const tokenGetter = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      return token;
+    } catch (error) {
+      await loginWithRedirect({
+        appState: {
+          // TODO?: url is as expected but app renders the "/" page
+          returnTo: router.latestLocation.href,
+        },
+      });
+    }
+  };
 
   return () =>
     fetcherWithAuth<ConnectionResponse>(
       "http://localhost:3000/auth/private/connection",
-      getAccessTokenSilently,
+      tokenGetter,
     );
 };
