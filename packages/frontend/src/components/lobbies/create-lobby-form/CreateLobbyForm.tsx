7bc7455989f7e1629239f22f9fd50f7da68894bb
jergauth <jeremiegthr@gmail.com>
2024-03-23 20:46:20 +0100

    fix: rm deprecate tanstack form feature

--- packages/frontend/src/components/lobbies/create-lobby-form/CreateLobbyForm.tsx
+++ packages/frontend/src/components/lobbies/create-lobby-form/CreateLobbyForm.tsx
@@ -39,70 +39,68 @@ export const CreateLobbyForm = ({ campaigns, socket }: Props) => {
     <div>
       <h2>Host a game</h2>
 
-      <form.Provider>
-        <form onSubmit={handleLobbyCreation}>
-          <div>
-            <form.Field
-              name="nbPlayersMax"
-              validatorAdapter={zodValidator}
-              validators={{
-                onChange: z
-                  .number()
-                  .min(2, "Your lobby must at least accept 2 players")
-                  .max(5, "Your lobby must at most accept 5 players"),
-              }}
-              children={(field) => (
-                <>
-                  <input
-                    type="number"
-                    name={field.name}
-                    value={field.state.value}
-                    onChange={(e) => field.handleChange(e.target.valueAsNumber)}
-                  />
-
-                  {field.state.meta.errors ? (
-                    <em>{field.state.meta.errors.join(", ")}</em>
-                  ) : null}
-                </>
-              )}
-            />
-          </div>
-
-          <div>
-            <form.Field
-              name="stageId"
-              validatorAdapter={zodValidator}
-              validators={{
-                onChange: z.string().uuid(),
-              }}
-              children={(field) => (
-                <select
+      <form onSubmit={handleLobbyCreation}>
+        <div>
+          <form.Field
+            name="nbPlayersMax"
+            validatorAdapter={zodValidator}
+            validators={{
+              onChange: z
+                .number()
+                .min(2, "Your lobby must at least accept 2 players")
+                .max(5, "Your lobby must at most accept 5 players"),
+            }}
+            children={(field) => (
+              <>
+                <input
+                  type="number"
                   name={field.name}
                   value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                >
-                  {campaigns.map((campaign) => (
-                    <option key={campaign.id} value={campaign.currentStage.id}>
-                      {campaign.currentStage.order}/{campaign.nbStages} --
-                      {campaign.title} --
-                      {campaign.currentStage.title}
-                    </option>
-                  ))}
-                </select>
-              )}
-            />
-          </div>
+                  onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+                />
 
-          <form.Subscribe
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
-            children={([canSubmit, isSubmitting]) => (
-              <button type="submit" disabled={!canSubmit}>
-                {isSubmitting ? "Your Lobby is being created" : "Create Lobby"}
-              </button>
+                {field.state.meta.errors ? (
+                  <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+              </>
             )}
           />
-        </form>
-      </form.Provider>
+        </div>
+
+        <div>
+          <form.Field
+            name="stageId"
+            validatorAdapter={zodValidator}
+            validators={{
+              onChange: z.string().uuid(),
+            }}
+            children={(field) => (
+              <select
+                name={field.name}
+                value={field.state.value}
+                onChange={(e) => field.handleChange(e.target.value)}
+              >
+                {campaigns.map((campaign) => (
+                  <option key={campaign.id} value={campaign.currentStage.id}>
+                    {campaign.currentStage.order}/{campaign.nbStages} --
+                    {campaign.title} --
+                    {campaign.currentStage.title}
+                  </option>
+                ))}
+              </select>
+            )}
+          />
+        </div>
+
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "Your Lobby is being created" : "Create Lobby"}
+            </button>
+          )}
+        />
+      </form>
     </div>
   );
 };
