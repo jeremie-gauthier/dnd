c45690e2db8e2b0551aea5b30a311d9fbfc79063
jergauth <jeremiegthr@gmail.com>
2024-03-26 23:51:39 +0100

    feat: add outlined variant to button

--- packages/frontend/src/components/shared/button/Button.tsx
+++ packages/frontend/src/components/shared/button/Button.tsx
@@ -1,11 +1,18 @@
 import { PropsWithChildren, ReactHTMLElement } from "react";
 
+const styleVariant: Readonly<Record<NonNullable<Props["variant"]>, string>> = {
+  primary: "bg-amber-800 hover:bg-amber-700 text-white p-2",
+  outlined:
+    "border-[1px] border-amber-800 text-amber-800 p-[7px] hover:border-amber-700 hover:text-amber-700 hover:bg-amber-700 hover:bg-opacity-5",
+};
+
 type ReactButtonProps = ReactHTMLElement<HTMLButtonElement>["props"];
 
 type Props = PropsWithChildren<{
   onClick?: ReactButtonProps["onClick"];
   type?: HTMLButtonElement["type"];
   disabled?: ReactButtonProps["disabled"];
+  variant?: "primary" | "outlined";
 }>;
 
 export const Button = ({
@@ -13,13 +20,14 @@ export const Button = ({
   type = "button",
   onClick,
   disabled,
+  variant = "primary",
 }: Props) => {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className="bg-amber-800 text-white p-2 rounded"
+      className={`${styleVariant[variant]} rounded font-medium text-sm ease-in-out duration-[250ms]`}
     >
       {children}
     </button>
