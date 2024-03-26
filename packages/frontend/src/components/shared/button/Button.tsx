14e9183fadd75392c30dff630b51e071828df109
jergauth <jeremiegthr@gmail.com>
2024-03-27 00:03:11 +0100

    feat: add button disabled style

--- packages/frontend/src/components/shared/button/Button.tsx
+++ packages/frontend/src/components/shared/button/Button.tsx
@@ -6,6 +6,13 @@ const styleVariant: Readonly<Record<NonNullable<Props["variant"]>, string>> = {
     "border-[1px] border-amber-800 text-amber-800 p-[7px] hover:border-amber-700 hover:text-amber-700 hover:bg-amber-700 hover:bg-opacity-5",
 };
 
+const styleVariantDisabled: Readonly<
+  Record<NonNullable<Props["variant"]>, string>
+> = {
+  primary: "bg-black bg-opacity-10 text-black/25 p-2",
+  outlined: "border-[1px] border-black/10 text-black/25 p-[7px]",
+};
+
 type ReactButtonProps = ReactHTMLElement<HTMLButtonElement>["props"];
 
 type Props = PropsWithChildren<{
@@ -27,7 +34,9 @@ export const Button = ({
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`${styleVariant[variant]} rounded font-medium text-sm ease-in-out duration-[250ms]`}
+      className={`${
+        disabled ? styleVariantDisabled[variant] : styleVariant[variant]
+      } rounded font-medium text-sm ease-in-out duration-[250ms]`}
     >
       {children}
     </button>
