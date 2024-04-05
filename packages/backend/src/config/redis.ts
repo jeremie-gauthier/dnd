abdc65a67d48aff62b92291f181b4c2ecd5377d4
jergauth <jeremiegthr@gmail.com>
2024-04-05 11:57:45 +0200

    config: redis url from env vars

--- packages/backend/src/config/redis.ts
+++ packages/backend/src/config/redis.ts
@@ -1,7 +1,12 @@
 import { registerAs } from "@nestjs/config";
+import { config as dotenvConfig } from "dotenv";
 import type { RedisClientOptions } from "redis";
 
+dotenvConfig({ path: ".env" });
+
 // TODO: create a username and a pwd before going live
-const config: RedisClientOptions = {};
+const config: RedisClientOptions = {
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+};
 
 export default registerAs("redis", () => config);
