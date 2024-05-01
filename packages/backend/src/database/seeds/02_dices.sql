9e4213cc9b3bb58f60ab3f52d037ccd0cec6c4f8
jergauth <jeremiegthr@gmail.com>
2024-05-01 22:54:53 +0200

    feat(db): add seeds for starter items

---
+++ packages/backend/src/database/seeds/02_dices.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  "dice" (
+    "name",
+    "color",
+    "values",
+    "min_value",
+    "max_value",
+    "mean_value"
+  )
+VALUES
+  ('yellow', '#fff700', '[0,0,1,1,1,1]', 0, 1, 0.66),
+  ('orange', '#ff6e00', '[1,1,1,1,2,2]', 1, 2, 1.33),
+  ('red', '#dc143c', '[0,0,1,2,2,3]', 0, 3, 1.33),
+  ('special', '#000000', '[0,0,0,1,1,1]', 0, 1, 0.5);
