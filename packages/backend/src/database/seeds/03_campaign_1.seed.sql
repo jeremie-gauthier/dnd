9b18625a890dd2eb5d90cf7d5631ded0b6b46dff
jergauth <jeremiegthr@gmail.com>
2024-05-08 18:43:10 +0200

    fix: campaigns map format

--- packages/backend/src/database/seeds/03_campaign_1.seed.sql
+++ packages/backend/src/database/seeds/03_campaign_1.seed.sql
@@ -1,130 +1,130 @@
 INSERT INTO
-	campaign (title, STATUS)
+  campaign (title, STATUS)
 VALUES
-	('Campagne 1', 'AVAILABLE');
+  ('Campagne 1', 'AVAILABLE');
 
 INSERT INTO
-	campaign_stage (
-		campaign_id,
-		"order",
-		title,
-		intro,
-		outro,
-		map_compiled,
-		STATUS
-	)
+  campaign_stage (
+    campaign_id,
+    "order",
+    title,
+    intro,
+    outro,
+    map_compiled,
+    STATUS
+  )
 VALUES
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		1,
-		'Les bandits gobelins',
-		'Le mal et les ténèbres se sont abattus sur le pays de Rallion et la région est devenue la proie des Monstres...',
-		'Félicitations ! Vous avez vaincu les bandits gobelins...',
-		'10;5     0,0;0,1;0,2;0,3;0,4      1,0;trap   2,0;wall     2,1;wall     2,2;door     2,3;wall     2,4;wall     4,4;pillar     8,0;wall     8,1;wall     8,2;wall     8,3;wall     8,4;wall',
-		'AVAILABLE'
-	),
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		2,
-		'Les bandits gobelins, le retour',
-		'Introduction du 2e stage',
-		'Conclusion du 2e stage',
-		'00T1001',
-		'AVAILABLE'
-	);
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    1,
+    'Les bandits gobelins',
+    'Le mal et les ténèbres se sont abattus sur le pays de Rallion et la région est devenue la proie des Monstres...',
+    'Félicitations ! Vous avez vaincu les bandits gobelins...',
+    '{"height":12,"width":5,"startingPositions":[{"row":2,"column":2},{"row":2,"column":3},{"row":2,"column":4},{"row":3,"column":2},{"row":3,"column":3},{"row":3,"column":4}],"entities":[{"row":5,"column":0,"kind":"wall"},{"row":5,"column":1,"kind":"wall"},{"row":5,"column":2,"kind":"wall"},{"row":5,"column":3,"kind":"door"},{"row":5,"column":4,"kind":"wall"}],"events":[{"name":"on_door_opening","doorCoord":{"row":5,"column":3},"action":"spawn_enemies","enemies":["goblin"],"startingTiles":[{"row":6,"column":0},{"row":7,"column":1},{"row":8,"column":2},{"row":7,"column":3},{"row":6,"column":4}]}]}',
+    'AVAILABLE'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    2,
+    'Les bandits gobelins, le retour',
+    'Introduction du 2e stage',
+    'Conclusion du 2e stage',
+    '{}',
+    'AVAILABLE'
+  );
 
 ;
 
 INSERT INTO
-	campaign_playable_heroes_hero_template (campaign_id, hero_template_id)
+  campaign_playable_heroes_hero_template (campaign_id, hero_template_id)
 VALUES
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		(
-			SELECT
-				id
-			FROM
-				hero_template
-			WHERE
-				name = 'Regdar'
-				AND LEVEL = 1
-		)
-	),
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		(
-			SELECT
-				id
-			FROM
-				hero_template
-			WHERE
-				name = 'Jozan'
-				AND LEVEL = 1
-		)
-	),
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		(
-			SELECT
-				id
-			FROM
-				hero_template
-			WHERE
-				name = 'Mialyë'
-				AND LEVEL = 1
-		)
-	),
-	(
-		(
-			SELECT
-				id
-			FROM
-				campaign
-			WHERE
-				title = 'Campagne 1'
-		),
-		(
-			SELECT
-				id
-			FROM
-				hero_template
-			WHERE
-				name = 'Lidda'
-				AND LEVEL = 1
-		)
-	);
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        hero_template
+      WHERE
+        name = 'Regdar'
+        AND LEVEL = 1
+    )
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        hero_template
+      WHERE
+        name = 'Jozan'
+        AND LEVEL = 1
+    )
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        hero_template
+      WHERE
+        name = 'Mialyë'
+        AND LEVEL = 1
+    )
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        campaign
+      WHERE
+        title = 'Campagne 1'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        hero_template
+      WHERE
+        name = 'Lidda'
+        AND LEVEL = 1
+    )
+  );
--- packages/backend/src/database/seeds/03_campaign_tutorial.seed.sql
+++ packages/backend/src/database/seeds/03_campaign_tutorial.seed.sql
@@ -27,19 +27,7 @@ VALUES
     'Le déplacement',
     'Apprenez à vous déplacer dans DnD',
     'Félicitations !',
-    '10;5
-    0,0;0,1;0,2;0,3;0,4
-    2,0;wall
-    2,1;wall
-    2,2;door
-    2,3;wall
-    2,4;wall
-    4,4;pillar
-    8,0;wall
-    8,1;wall
-    8,2;wall
-    8,3;wall
-    8,4;wall',
+    '{}',
     'AVAILABLE'
   );
 
