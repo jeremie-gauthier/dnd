fb2b95e9413a68037cd44624482c6df57faf193d
jergauth <jeremiegthr@gmail.com>
2024-05-09 14:16:23 +0200

    refacto: split state machine gateway into pub/sub gateways

--- packages/backend/src/game/state-machine/state-machine.publisher-gateway.ts
+++ packages/backend/src/game/state-machine/state-machine.publisher-gateway.ts
@@ -1,43 +1,16 @@
-import { ClientGameEvent, GameEntity, ServerGameEvent } from "@dnd/shared";
+import { GameEntity, ServerGameEvent } from "@dnd/shared";
 import { OnEvent } from "@nestjs/event-emitter";
-import {
-  ConnectedSocket,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from "@nestjs/websockets";
-import type { ServerSocket, WsServer } from "src/types/socket.type";
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import type { WsServer } from "src/types/socket.type";
 import { GameEvent } from "../events/emitters/game-events.enum";
-import { EndPlayerTurnUseCase } from "./end-player-turn/end-player-turn.uc";
 import { PlayerStateService } from "./services/player-state/player-state.service";
 
-@WebSocketGateway({
-  cors: {
-    origin: "*",
-  },
-})
-export class StateMachinePrivateGateway {
-  constructor(
-    private readonly endPlayerTurnUseCase: EndPlayerTurnUseCase,
-    private readonly playerStateService: PlayerStateService,
-  ) {}
+@WebSocketGateway()
+export class StateMachinePublisherGateway {
+  constructor(private readonly playerStateService: PlayerStateService) {}
 
   @WebSocketServer()
   private readonly server: WsServer;
-
-  private getMessageContext(client: ServerSocket) {
-    return {
-      server: this.server,
-      client,
-    };
-  }
-
-  @SubscribeMessage(ClientGameEvent.PlayableEntityTurnEnds)
-  public async endPlayerTurn(
-    @ConnectedSocket() client: ServerSocket,
-  ): Promise<void> {
-    await this.endPlayerTurnUseCase.execute({ userId: client.data.userId });
-  }
 
   @OnEvent(GameEvent.EnemiesSpawned)
   @OnEvent(GameEvent.DoorOpened)
