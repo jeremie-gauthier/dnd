d7710bcb8945f082dfb6e4affc24cb294ffb34d2
jergauth <jeremiegthr@gmail.com>
2024-05-23 20:59:34 +0200

    feat: define i18n config

---
+++ packages/frontend/src/config/i18n.ts
@@ -0,0 +1,43 @@
+import { GetTranslationOutput } from "@dnd/shared";
+import i18next from "i18next";
+import HttpApi, { HttpBackendOptions } from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18next
+  .use(HttpApi)
+  .use(initReactI18next)
+  .init<HttpBackendOptions>({
+    backend: {
+      reloadInterval: false,
+      request: async (_options, url, _payload, callback) => {
+        try {
+          const locale = url.split("/")[2];
+          if (!locale.includes("-")) {
+            return callback(undefined, { status: 200, data: {} });
+          }
+
+          const namespace = url.split("/")[3].replace(".json", "");
+          const requestUrl = `http://localhost:3000/translation/public/get-translation/${locale}/${namespace}`;
+          const response = await fetch(requestUrl, { method: "GET" });
+          const data: GetTranslationOutput = await response.json();
+          return callback(undefined, {
+            status: 200,
+            data: data.translations,
+          });
+        } catch (error) {
+          return callback(error, null!);
+        }
+      },
+    },
+    lng: "fr-FR",
+    fallbackLng: "fr-FR",
+    debug: false,
+    ns: ["common"],
+    defaultNS: "common",
+    fallbackNS: "common",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18next;
