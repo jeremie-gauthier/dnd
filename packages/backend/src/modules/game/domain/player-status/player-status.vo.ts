471e46ec60c5865d4131862921f047651dc53399
jergauth <jeremiegthr@gmail.com>
2024-06-25 19:20:17 +0200

    feat: add PlayerStatus VO

---
+++ packages/backend/src/modules/game/domain/player-status/player-status.vo.ts
@@ -0,0 +1,59 @@
+import { ValueObject } from "src/modules/shared/domain/value-object";
+import { PlayerStatusError } from "./player-status.error";
+
+type Data = {
+  currentPhase: "PREPARATION" | "IDLE" | "ACTION";
+};
+
+export class PlayerStatus extends ValueObject<Data> {
+  private static STATE_MACHINE: Readonly<
+    Record<
+      Data["currentPhase"],
+      {
+        advanceTo: Data["currentPhase"][];
+        rollbackFrom: Data["currentPhase"][];
+      }
+    >
+  > = {
+    PREPARATION: {
+      advanceTo: ["IDLE"],
+      rollbackFrom: ["IDLE", "PREPARATION"],
+    },
+    IDLE: {
+      advanceTo: ["ACTION"],
+      rollbackFrom: [],
+    },
+    ACTION: {
+      advanceTo: ["IDLE"],
+      rollbackFrom: [],
+    },
+  };
+
+  public equals(other: PlayerStatus): boolean {
+    return this._data.currentPhase === other._data.currentPhase;
+  }
+
+  public get current() {
+    return this._data.currentPhase;
+  }
+
+  public advanceTo({
+    currentPhase,
+  }: { currentPhase: Data["currentPhase"] }): PlayerStatus {
+    this.mustBeAbleToAdvanceTo({ currentPhase });
+    return new PlayerStatus({ currentPhase });
+  }
+
+  private mustBeAbleToAdvanceTo({
+    currentPhase,
+  }: { currentPhase: Data["currentPhase"] }) {
+    const canAdvanceTo =
+      PlayerStatus.STATE_MACHINE[this.current].advanceTo.includes(currentPhase);
+    if (!canAdvanceTo) {
+      throw new PlayerStatusError({
+        name: "ILLEGAL_STATUS_EDIT",
+        message: `Lobby status cannot advance to '${currentPhase}' (current: '${this.current}')`,
+      });
+    }
+  }
+}
