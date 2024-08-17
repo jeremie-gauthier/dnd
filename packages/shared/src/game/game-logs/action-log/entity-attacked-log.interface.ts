bbf32e7eee9bd569c01a3eb69638425918ac8fd1
jergauth <jeremiegthr@gmail.com>
2024-08-17 16:45:09 +0200

    feat: augment interface to more details on attacks

--- packages/shared/src/game/game-logs/action-log/entity-attacked-log.interface.ts
+++ packages/shared/src/game/game-logs/action-log/entity-attacked-log.interface.ts
@@ -6,6 +6,8 @@ export interface EntityAttackedActionLog extends ActionLog {
   data: {
     attackerEntityName: PlayableEntity["name"];
     attackItemUsedName: GameItem["name"];
+    bonusScore: number;
+    diceScore: number;
     diceRollResults: { name: string; color: `#${string}`; result: number }[];
     attackPower: number;
     targetEntityName: PlayableEntity["name"];
