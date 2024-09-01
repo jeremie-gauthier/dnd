789da7e75100fd0c3c64f3172c88aa959d014765
jergauth <jeremiegthr@gmail.com>
2024-09-01 18:23:26 +0200

    fix: issue preventing to swap 2 items

--- packages/shared/src/game/playable-entity-swap-items/playable-entity-swap-items-input.schema.ts
+++ packages/shared/src/game/playable-entity-swap-items/playable-entity-swap-items-input.schema.ts
@@ -1,17 +1,10 @@
 import { z } from "zod";
 
-export const playableEntitySwapItemsInputSchema = z
-  .object({
-    gameId: z.string().uuid(),
-    gearItemId: z.string().optional(),
-    backpackItemId: z.string().optional(),
-  })
-  .refine(
-    (schema) =>
-      (schema.backpackItemId && !schema.gearItemId) ||
-      (!schema.backpackItemId && schema.gearItemId),
-    "At least one itemId is required",
-  );
+export const playableEntitySwapItemsInputSchema = z.object({
+  gameId: z.string().uuid(),
+  gearItemId: z.string().optional(),
+  backpackItemId: z.string().optional(),
+});
 
 export type PlayableEntitySwapItemsInput = z.infer<
   typeof playableEntitySwapItemsInputSchema
