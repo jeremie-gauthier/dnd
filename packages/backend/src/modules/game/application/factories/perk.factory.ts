9d05db7cb1b571879e04e59e069c68c0164c71c5
jergauth <jeremiegthr@gmail.com>
2024-09-13 20:11:15 +0200

    feat: add critical failure perk

--- packages/backend/src/modules/game/application/factories/perk.factory.ts
+++ packages/backend/src/modules/game/application/factories/perk.factory.ts
@@ -1,4 +1,5 @@
 import { Breakable } from "../../domain/perk/breakable.perk";
+import { CriticalFailure } from "../../domain/perk/critical-failure.perk";
 import { ManaLeech } from "../../domain/perk/mana-leech.perk";
 import { Perk } from "../../domain/perk/perk.abstract";
 import { GameItem } from "./item.interface";
@@ -14,6 +15,8 @@ export class PerkFactory {
         return new ManaLeech(data);
       case "breakable":
         return new Breakable(data);
+      case "critical_failure":
+        return new CriticalFailure(data);
       default:
         throw new Error(`No "${data.name}" perk found`);
     }
---
+++ packages/backend/src/modules/game/domain/perk/critical-failure.perk.ts
@@ -0,0 +1,17 @@
+import { Attack } from "../attack/attack.entity";
+import { Item } from "../item/item.abstract";
+import { Playable } from "../playable-entities/playable-entity/playable-entity.abstract";
+import { Perk } from "./perk.abstract";
+
+export class CriticalFailure extends Perk {
+  public apply({
+    dicesResults,
+  }: {
+    dicesResults: ReturnType<Attack["roll"]>;
+    itemUsed: Item;
+    attacker: Playable;
+    defender: Playable;
+  }): void {
+    dicesResults.sumResult = 0;
+  }
+}
