90a1de8ca89461b0500fbc64e0b6bb1ca9dec425
jergauth <jeremiegthr@gmail.com>
2024-09-23 22:13:54 +0200

    fixup with "feat: add potion item domain"

--- packages/backend/src/modules/game/infra/database/item/item.mapper.ts
+++ packages/backend/src/modules/game/infra/database/item/item.mapper.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { AttackDice } from "src/database/entities/attack-dice.entity";
 import { ChestTrap as ChestTrapPersistence } from "src/database/entities/chest-trap.entity";
 import { Item as ItemPersistence } from "src/database/entities/item.entity";
+import { Potion as PotionPersistence } from "src/database/entities/potion.entity";
 import { Spell as SpellPersistence } from "src/database/entities/spell.entity";
 import { Weapon as WeaponPersistence } from "src/database/entities/weapon.entity";
 import { ItemFactory } from "src/modules/game/application/factories/item.factory";
@@ -21,7 +22,11 @@ export class ItemMapper extends Mapper<ItemPersistence, ItemDomain> {
   }
 
   public toDomain(
-    persistence: WeaponPersistence | SpellPersistence | ChestTrapPersistence,
+    persistence:
+      | WeaponPersistence
+      | SpellPersistence
+      | ChestTrapPersistence
+      | PotionPersistence,
   ): ItemDomain {
     return ItemFactory.create({
       ...persistence,
