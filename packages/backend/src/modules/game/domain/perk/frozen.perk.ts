5a6f25d29aa2a062414a7cf3a68bbfd9d808517b
jergauth <jeremiegthr@gmail.com>
2024-09-29 23:07:20 +0200

    feat: implement Frozen Perk

--- packages/backend/src/modules/game/domain/perk/frozen.perk.ts
+++ packages/backend/src/modules/game/domain/perk/frozen.perk.ts
@@ -1,5 +1,6 @@
 import { Attack } from "../attack/attack.entity";
 import { Item } from "../item/item.abstract";
+import { Stopped } from "../playable-entities/playable-entity/conditions/condition/stopped.condition";
 import { Playable } from "../playable-entities/playable-entity/playable-entity.abstract";
 import { Perk } from "./perk.abstract";
 
@@ -8,10 +9,18 @@ export class Frozen extends Perk {
     super({ name: "frozen", trigger: "special_dice" });
   }
 
-  public apply(_: {
+  public apply({
+    defender,
+  }: {
     dicesResults: ReturnType<Attack["roll"]>;
     itemUsed: Item;
     attacker: Playable;
     defender: Playable;
-  }): void {}
+  }): void {
+    if (!defender.isUndead()) {
+      defender.conditions.add({
+        condition: new Stopped({ remainingTurns: 1 }),
+      });
+    }
+  }
 }
