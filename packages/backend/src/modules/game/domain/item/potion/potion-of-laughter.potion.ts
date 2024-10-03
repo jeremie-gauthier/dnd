da58777446487d99b56f651c2f61cba9566f947a
jergauth <jeremiegthr@gmail.com>
2024-10-03 19:54:35 +0200

    feat: implement potion of laughter

--- packages/backend/src/modules/game/domain/item/potion/potion-of-laughter.potion.ts
+++ packages/backend/src/modules/game/domain/item/potion/potion-of-laughter.potion.ts
@@ -1,16 +1,33 @@
 import { Game } from "../../game/game.aggregate";
+import { Stopped } from "../../playable-entities/playable-entity/conditions/condition/stopped.condition";
 import { Hero } from "../../playable-entities/playable-entity/heroes/hero.abstract";
 import { Potion } from "./potion.abstract";
+import { PotionError } from "./potion.error";
 
 export class PotionOfLaughter extends Potion {
   constructor() {
     super({ level: 1, name: "potion_of_laughter_1" });
   }
 
-  public use(_: {
+  public use({
+    game,
+  }: {
     playableEntity: Hero;
     game: Game;
   }): void {
-    throw new Error("Method not implemented.");
+    const monsters = game.playableEntities.getMonsters();
+    const randIndex = Math.trunc(Math.random() * monsters.length);
+    const randomMonster = monsters[randIndex];
+
+    if (!randomMonster) {
+      throw new PotionError({
+        name: "INVALID_USAGE",
+        message: "No Monster alive",
+      });
+    }
+
+    randomMonster.conditions.add({
+      condition: new Stopped({ remainingTurns: 1 }),
+    });
   }
 }
