8a26daf0b05390e787ec82a96ee62cb72e7f093b
jergauth <jeremiegthr@gmail.com>
2024-10-03 20:15:49 +0200

    feat: implement potion of weakness

--- packages/backend/src/modules/game/domain/item/potion/potion-of-weakness.potion.ts
+++ packages/backend/src/modules/game/domain/item/potion/potion-of-weakness.potion.ts
@@ -1,16 +1,35 @@
 import { Game } from "../../game/game.aggregate";
+import { Weakness } from "../../playable-entities/playable-entity/conditions/condition/weakness.condition";
 import { Hero } from "../../playable-entities/playable-entity/heroes/hero.abstract";
 import { Potion } from "./potion.abstract";
+import { PotionError } from "./potion.error";
 
 export class PotionOfWeakness extends Potion {
   constructor() {
     super({ level: 1, name: "potion_of_weakness_1" });
   }
 
-  public use(_: {
+  public use({
+    game,
+  }: {
     playableEntity: Hero;
     game: Game;
   }): void {
-    throw new Error("Method not implemented.");
+    const monsters = game.playableEntities.getMonsters();
+    const randIndex = Math.trunc(Math.random() * monsters.length);
+    const randomMonster = monsters[randIndex];
+
+    if (!randomMonster) {
+      throw new PotionError({
+        name: "INVALID_USAGE",
+        message: "No Monster alive",
+      });
+    }
+
+    const weaknessCondition = new Weakness({ remainingTurns: 2 });
+    weaknessCondition.apply({ playableEntityAffected: randomMonster });
+    randomMonster.conditions.add({
+      condition: weaknessCondition,
+    });
   }
 }
