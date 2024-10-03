4de3ac3eef60d7058dfba254267b99aa2c8e1fcb
jergauth <jeremiegthr@gmail.com>
2024-10-03 20:15:38 +0200

    feat: add weakness condition

--- packages/backend/src/modules/game/application/factories/condition.factory.ts
+++ packages/backend/src/modules/game/application/factories/condition.factory.ts
@@ -1,5 +1,7 @@
+import { BrokenArmor } from "../../domain/playable-entities/playable-entity/conditions/condition/broken-armor.condition";
 import { Condition } from "../../domain/playable-entities/playable-entity/conditions/condition/condition.abstract";
 import { Stopped } from "../../domain/playable-entities/playable-entity/conditions/condition/stopped.condition";
+import { Weakness } from "../../domain/playable-entities/playable-entity/conditions/condition/weakness.condition";
 import { PlayableEntityCondition } from "../../infra/database/game/model/playable-entity/condition.type";
 
 export class ConditionFactory {
@@ -9,6 +11,10 @@ export class ConditionFactory {
     switch (data.name) {
       case "stopped":
         return new Stopped(data);
+      case "brokenArmor":
+        return new BrokenArmor(data);
+      case "weakness":
+        return new Weakness(data);
       default:
         throw new Error(`No "${data.name}" Condition found`);
     }
