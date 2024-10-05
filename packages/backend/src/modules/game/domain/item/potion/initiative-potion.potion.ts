a112a09946c07edf819f5b6bc63179fc37ce5d9e
jergauth <jeremiegthr@gmail.com>
2024-10-05 09:58:35 +0200

    feat: implement initiative potion

--- packages/backend/src/modules/game/domain/item/potion/initiative-potion.potion.ts
+++ packages/backend/src/modules/game/domain/item/potion/initiative-potion.potion.ts
@@ -1,16 +1,31 @@
 import { Game } from "../../game/game.aggregate";
 import { Hero } from "../../playable-entities/playable-entity/heroes/hero.abstract";
 import { Potion } from "./potion.abstract";
+import { PotionError } from "./potion.error";
 
 export class InitiativePotion extends Potion {
   constructor() {
     super({ level: 1, name: "initiative_potion_1" });
   }
 
-  public use(_: {
+  public use({
+    playableEntity,
+    game,
+  }: {
     playableEntity: Hero;
     game: Game;
   }): void {
-    throw new Error("Method not implemented.");
+    const monsters = game.playableEntities.getMonsters();
+    if (monsters.length === 0) {
+      throw new PotionError({
+        name: "INVALID_USAGE",
+        message: "No Monster alive",
+      });
+    }
+
+    const nextMonsterToPlay = monsters[0]!;
+    const heroInitiativeValue = playableEntity.initiative.value;
+    playableEntity.setInitiative(nextMonsterToPlay.initiative.value);
+    nextMonsterToPlay.setInitiative(heroInitiativeValue);
   }
 }
