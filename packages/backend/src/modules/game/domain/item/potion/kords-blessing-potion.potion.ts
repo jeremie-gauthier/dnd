a654eca522206624fae6b13da954963b81ef9664
jergauth <jeremiegthr@gmail.com>
2024-10-05 10:20:31 +0200

    feat: implement kord benediction potion

--- packages/backend/src/modules/game/domain/item/potion/kords-blessing-potion.potion.ts
+++ packages/backend/src/modules/game/domain/item/potion/kords-blessing-potion.potion.ts
@@ -1,4 +1,5 @@
 import { Game } from "../../game/game.aggregate";
+import { DoubleWeaponDamage } from "../../playable-entities/playable-entity/conditions/condition/double-weapon-damage.condition";
 import { Hero } from "../../playable-entities/playable-entity/heroes/hero.abstract";
 import { Potion } from "./potion.abstract";
 
@@ -7,10 +8,14 @@ export class KordsBlessingPotion extends Potion {
     super({ level: 1, name: "kords_blessing_potion_1" });
   }
 
-  public use(_: {
+  public use({
+    playableEntity,
+  }: {
     playableEntity: Hero;
     game: Game;
   }): void {
-    throw new Error("Method not implemented.");
+    playableEntity.conditions.add({
+      condition: new DoubleWeaponDamage({ remainingTurns: 1 }),
+    });
   }
 }
