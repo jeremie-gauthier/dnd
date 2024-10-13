5ae1a0a1b75719a1eec03858492088c3861edd46
jergauth <jeremiegthr@gmail.com>
2024-10-13 14:56:08 +0200

    feat: add hover opacity on action log list

--- packages/frontend/src/features/game/components/action-log/action-log-list.component.tsx
+++ packages/frontend/src/features/game/components/action-log/action-log-list.component.tsx
@@ -5,7 +5,7 @@ export const ActionsLog = () => {
   const { actionsLogs } = useGameContext();
 
   return (
-    <ul className="absolute bottom-4 left-4 flex flex-col-reverse h-32 w-96 overflow-y-scroll rounded-sm px-1 bg-primary-600 text-white">
+    <ul className="absolute bottom-4 left-4 flex flex-col-reverse h-32 w-96 overflow-y-scroll rounded-sm px-1 bg-primary-600 text-white opacity-75 duration-300 hover:opacity-100">
       {actionsLogs.map((actionLog, idx) => (
         <ActionLogItem
           key={`${actionLog.type}:${actionLog.createdAt.valueOf()}:${idx}`}
