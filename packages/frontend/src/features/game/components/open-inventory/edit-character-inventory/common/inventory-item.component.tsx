ed11d4f0ac9f15ae1db380681d1d26a30c1bab34
jergauth <jeremiegthr@gmail.com>
2024-10-22 23:10:38 +0200

    chores: rm item layout in inventory

--- packages/frontend/src/features/game/components/open-inventory/edit-character-inventory/common/inventory-item.component.tsx
+++ packages/frontend/src/features/game/components/open-inventory/edit-character-inventory/common/inventory-item.component.tsx
@@ -8,7 +8,6 @@ import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { GameItem } from "@dnd/shared";
 import { InspectItem } from "@features/game/components/inspect-item/inspect-item.component";
-import { useTranslation } from "react-i18next";
 
 type Props = {
   item: GameItem;
@@ -16,7 +15,6 @@ type Props = {
 };
 
 export const InventoryItem = ({ item, storageSpace }: Props) => {
-  const { t } = useTranslation(["items"]);
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: `draggable-item-${item.name}`,
     data: { item, storageSpace },
@@ -36,9 +34,6 @@ export const InventoryItem = ({ item, storageSpace }: Props) => {
           {...attributes}
         >
           <img src={item.imgUrl} alt={item.name} className="rounded" />
-          <div className="absolute hidden inset-0 bg-black bg-opacity-35 text-white text-sm group-hover:flex flex-col p-1">
-            <p>{t(item.name)}</p>
-          </div>
         </TooltipTrigger>
 
         <TooltipContent
