455734fcfd2be1a52c9487e2617a8af1fa5f6bcb
jergauth <jeremiegthr@gmail.com>
2025-01-20 12:34:51 +0100

    feat: add pathfinder algorithm

---
+++ packages/backend/src/modules/game/domain/services/pathfinder/binary-heap.ts
@@ -0,0 +1,190 @@
+/**
+	The type of function used to make heap's nodes comparisons.
+	@param a A node of the heap.
+	@param b Another node of the heap.
+	@template TData The heap's nodes type.
+ */
+type Comparator<TData> = (a: TData, b: TData) => number;
+
+/**
+ * A data-structure class to represent a binary heap.
+ * @template TData The heap's nodes type.
+ * @link https://en.wikipedia.org/wiki/Binary_heap
+ */
+export class BinaryHeap<TData extends { id: string | number }> {
+  private readonly data: Array<TData> = [];
+  private size = 0;
+  private existingNodes = new Map<TData["id"], number>();
+
+  /**
+   * Construct a binary heap object.
+   * @param comparator The function used to make heap's nodes comparisons.
+   * @template TData The heap's nodes type.
+   */
+  constructor(private readonly comparator: Comparator<TData>) {}
+
+  private swap(idxA: number, idxB: number) {
+    const tmpA = this.data[idxA]!;
+    const tmpB = this.data[idxB]!;
+
+    // @ts-expect-error under control idx
+    this.data[idxA] = this.data[idxB];
+    this.data[idxB] = tmpA;
+
+    this.existingNodes.set(tmpA.id, idxB);
+    this.existingNodes.set(tmpB.id, idxA);
+  }
+
+  private getParentIdx(idx: number) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  private getLeftChildIdx(idx: number) {
+    return 2 * idx + 1;
+  }
+
+  private getRightChildIdx(idx: number) {
+    return 2 * idx + 2;
+  }
+
+  private heapifyUp(startingIdx: number) {
+    let parentIdx = this.getParentIdx(startingIdx);
+    let currentIdx = startingIdx;
+
+    while (
+      currentIdx > 0 &&
+      this.comparator(this.data[currentIdx]!, this.data[parentIdx]!) < 0
+    ) {
+      this.swap(currentIdx, parentIdx);
+      currentIdx = parentIdx;
+      parentIdx = this.getParentIdx(currentIdx);
+    }
+  }
+
+  private heapifyDown(startingIdx: number) {
+    const left = this.getLeftChildIdx(startingIdx);
+    const right = this.getRightChildIdx(startingIdx);
+    let smallest = startingIdx;
+
+    if (
+      left < this.size &&
+      this.comparator(this.data[left]!, this.data[startingIdx]!) < 0
+    ) {
+      smallest = left;
+    }
+
+    if (
+      right < this.size &&
+      this.comparator(this.data[right]!, this.data[smallest]!) < 0
+    ) {
+      smallest = right;
+    }
+
+    if (smallest !== startingIdx) {
+      this.swap(startingIdx, smallest);
+      this.heapifyDown(smallest);
+    }
+  }
+
+  /**
+   * Insert a node to the heap.
+   * @param element The node to insert in the heap.
+   */
+  public insert(element: TData) {
+    this.data[this.size] = element;
+    this.existingNodes.set(element.id, this.size);
+    this.size += 1;
+    this.heapifyUp(this.size - 1);
+  }
+
+  /**
+   * Pop the first node from the heap and sort the tree accordingly.
+   * @returns A node or undefined if the heap is empty.
+   */
+  public extract() {
+    if (this.size === 0) {
+      return;
+    }
+    if (this.size === 1) {
+      this.size -= 1;
+      const data = this.data[this.size]!;
+      // @ts-expect-error under control idx
+      this.data[this.size] = undefined;
+      this.existingNodes.delete(data.id);
+      return data;
+    }
+
+    const root = this.data[0]!;
+    this.size -= 1;
+    // @ts-expect-error under control idx
+    this.data[0] = this.data[this.size];
+    // @ts-expect-error under control idx
+    this.data[this.size] = undefined;
+
+    this.existingNodes.delete(root.id);
+
+    this.heapifyDown(0);
+
+    return root;
+  }
+
+  /**
+   * Delete a node from the heap and sort the tree accordingly.
+   * @param id The id of the element to delete.
+   */
+  public delete(id: TData["id"]) {
+    const elementIdx = this.existingNodes.get(id);
+    if (elementIdx === undefined) return;
+
+    const element = this.data[elementIdx]!;
+
+    this.size -= 1;
+    this.swap(elementIdx, this.size);
+    // @ts-expect-error under control idx
+    this.data[this.size] = undefined;
+
+    this.existingNodes.delete(element.id);
+
+    const comparison = this.comparator(this.data[elementIdx]!, element);
+    if (comparison < 0) {
+      this.heapifyUp(elementIdx);
+    } else if (comparison > 0) {
+      this.heapifyDown(elementIdx);
+    }
+  }
+
+  /**
+   * Search a node from the heap.
+   * @param id The id of the searched element.
+   * @returns The searched node or undefined if nothing match findPredicate.
+   */
+  public search(id: TData["id"]) {
+    const elementIdx = this.existingNodes.get(id);
+    if (elementIdx === undefined) return;
+    return this.data[elementIdx];
+  }
+
+  /**
+   * Decrease the value of a node in the heap and sort the tree accordingly.
+   * @param data The data of the element to update.
+   */
+  public decreaseElement(data: TData) {
+    const elementIdx = this.existingNodes.get(data.id);
+    if (elementIdx === undefined) return;
+
+    this.data[elementIdx] = data;
+    this.heapifyUp(elementIdx);
+  }
+
+  /**
+   * Increase the value of a node in the heap and sort the tree accordingly.
+   * @param data The data of the element to update.
+   */
+  public increaseElement(data: TData) {
+    const elementIdx = this.existingNodes.get(data.id);
+    if (elementIdx === undefined) return;
+
+    this.data[elementIdx] = data;
+    this.heapifyDown(elementIdx);
+  }
+}
---
+++ packages/backend/src/modules/game/domain/services/pathfinder/pathfinder.ts
@@ -0,0 +1,111 @@
+import { BinaryHeap } from "./binary-heap";
+import type { Coord } from "./interfaces/coord.interface";
+import { Node } from "./node";
+
+function reconstructPath<TData>(arrivalNode: Node<TData>) {
+  const path: Array<Coord> = [];
+
+  let current: Node<TData> | undefined = arrivalNode;
+  while (current) {
+    path.push(current.coord);
+    current = current.parent;
+  }
+  path.reverse();
+  return path;
+}
+
+/**
+ *The type of function used to tell whether the goal node has been reached or not.
+ *	@param node The node to evaluate.
+ *	@param coord The coord of the node being evaluated.
+ *	@template TData Represents a grid's Cell type.
+ */
+type GoalPredicate<TData> = (node: TData, coord: Coord) => boolean;
+
+/**
+ *	The A* heuristic.
+ *	@param node The node to evaluate.
+ *	@param coord The coord of the node being evaluated.
+ *	@template TData Represents a grid's Cell type.
+ */
+type Heuristic<TData> = (node: TData, coord: Coord) => number;
+
+/**
+ * The optional options object.
+ * @template TData Represents a grid's Cell type.
+ */
+type Options<TData> = {
+  /**
+   * The optional predicate to tell whether a cell is visitable by the algorithm or not.
+   * @param node The node to evaluate.
+   * @template TData Represents a grid's Cell type.
+   */
+  canVisitNode?: (node: TData) => boolean;
+};
+
+/**
+ * A pathfinder algorithm implemented with A*.
+ * @param grid The grid to work on represented as a 2D-array.
+ * @param startCoord The starting coordinate for the algorithm.
+ * @param hasReachGoal The predicate that tells whether the goal node has been reached or not.
+ * @param heuristic The A* heuristic.
+ * @param options Options to customize the A* algorithm.
+ * @template TData Represents a grid's Cell type.
+ * @link https://en.wikipedia.org/wiki/A*_search_algorithm
+ */
+export function pathfinder<TData>({
+  grid,
+  startCoord,
+  hasReachGoal,
+  heuristic,
+  options,
+}: {
+  grid: Array<Array<TData>>;
+  startCoord: Coord;
+  hasReachGoal: GoalPredicate<TData>;
+  heuristic: Heuristic<TData>;
+  options?: Options<TData>;
+}) {
+  const openSet = new BinaryHeap<Node<TData>>(
+    (a, b) => a.heuristic - b.heuristic,
+  );
+  const startNode = new Node(startCoord, grid, 0, 0);
+  openSet.insert(startNode);
+
+  const closedSet = new Set<Node["id"]>([startNode.id]);
+
+  let current = openSet.extract();
+  while (current) {
+    if (hasReachGoal(current.data, current.coord)) {
+      const path = reconstructPath(current);
+      return path;
+    }
+
+    const neighbourNodes = current.getNeighbourNodes();
+    for (const neighbourNode of neighbourNodes) {
+      // neighbour node has already been visited
+      if (closedSet.has(neighbourNode.id)) continue;
+
+      if (options?.canVisitNode && !options.canVisitNode(neighbourNode.data)) {
+        continue;
+      }
+
+      neighbourNode.heuristic =
+        neighbourNode.cost + heuristic(neighbourNode.data, neighbourNode.coord);
+
+      const neighbourNodeInOpenSet = openSet.search(neighbourNode.id);
+      // neighbour node is not planed to be visited yet
+      if (!neighbourNodeInOpenSet) {
+        openSet.insert(neighbourNode);
+      }
+      // neighbour node is planed to be visited via a worse path
+      else if (neighbourNodeInOpenSet.heuristic > neighbourNode.heuristic) {
+        // we record this better path we just found
+        openSet.decreaseElement(neighbourNode);
+      }
+    }
+
+    closedSet.add(current.id);
+    current = openSet.extract();
+  }
+}
