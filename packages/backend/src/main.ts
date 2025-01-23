c2932028e9effc0dd25e5c770c6327edcf7657f5
jergauth <jeremiegthr@gmail.com>
2025-01-23 22:21:14 +0100

    feat: swagger config

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -4,6 +4,7 @@ dotenvConfig({ path: ".env" });
 import "multer";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -16,6 +17,14 @@ async function bootstrap() {
   app.enableShutdownHooks();
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("DnD")
+    .setDescription("The DnD API description")
+    .setVersion("1.0")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
 
   const configService = app.get(ConfigService);
   const PORT = configService.getOrThrow<string>("PORT");
